b897dda262dae9110e83126cb147de6cc4429089
mohithv <mohith9805@gmail.com>
2026-04-02 15:46:32 +0530

    v1 updating finnhub api

--- src/clients/finnhubClient.ts
+++ src/clients/finnhubClient.ts
@@ -1,17 +1,31 @@
 import axios from "axios";
-import { symbol } from "zod";
 
 const BASE_URL = "https://finnhub.io/api/v1";
+
+type FinnhubQuoteResponse = {
+    c: number;
+    d: number;
+    dp: number;
+    h: number;
+    l: number;
+    o: number;
+    pc: number;
+    t: number;
+};
 
 export const getStockQuote = async (symbol: string) => {
 
     const apiKey = process.env.FINNHUB_API_KEY;
+    if (!apiKey || apiKey.trim().length === 0) {
+        throw new Error("Missing FINNHUB_API_KEY env var");
+    }
 
-    const response = await axios.get(`${BASE_URL}/quote`, {
+    const response = await axios.get<FinnhubQuoteResponse>(`${BASE_URL}/quote`, {
         params: {
             symbol,
             token: apiKey,
         },
     });
+    console.log("finnhub api:", response.data);
     return response.data;
 }
